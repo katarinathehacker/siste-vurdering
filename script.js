8e8f9381b72b9ec50125e2770c91e1b638662fbd
katarinathehacker <katarina.thi@gmail.com>
2025-05-30 12:36:58 +0200

    ferdig med countdown

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// Nav baren 
-const navLister = document.querySelectorAll(".nav-liste");
 
-navLister.forEach(liste=> {
-    const h3 = liste.querySelector("h3");
-    const dropdown = liste.querySelector(".dropdown-content");
-
-    if(dropdown) {
-        h3.addEventListener("click", () => {
-            liste.classList.toggle("active");
-        });
-    }
-});
-// avslutt js til nav bar
 
 // nedtelling til ferie 
 const dagerEl = document.getElementById("dager");
